24d5107dac9a619f876a384960f87feb4b0ae993
Lukasz Plac <Lukasz.Plac@woodward.com>
2018-03-02 16:57:22 +0100

    working on authentication

---
+++ src/store/actions/auth.js
@@ -0,0 +1,61 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+	return {
+		type: actionTypes.AUTH_START
+	};
+};
+
+export const authSuccess = (token, userId) => {
+	return {
+		type: actionTypes.AUTH_SUCCESS,
+		idToken: token,
+		userId: userId
+	};
+};
+
+export const authFail = (error) => {
+	return {
+		type: actionTypes.AUTH_FAIL,
+		error: error
+	};
+};
+
+export const logout = () => {
+	return {
+		type: actionTypes.AUTH_LOGOUT
+	}
+}
+
+export const checkAuthTimeout = (time) => {
+	return dispatch => {
+		setTimeout(() => dispatch(logout()), time * 1000);
+	}
+}
+
+export const auth = (email, password, isSignup) => {
+	console.log(email + ' ' + password);
+	return dispatch => {
+		dispatch(authStart());
+		const authData = {
+			email: email,
+			password: password,
+			returnSecureToken: true
+		};
+		let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBvurObY9TeJjF5w-SMlicJrlf-jxch6pc';
+		if (!isSignup) {
+			url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBvurObY9TeJjF5w-SMlicJrlf-jxch6pc';	
+		}
+		axios.post(url, authData)
+			.then(response => {
+				console.log(response.data);
+				dispatch(authSuccess(response.data.idToken, response.data.localId));
+				dispatch(checkAuthTimeout(response.data.expiresIn))
+			})
+			.catch(err => {
+				console.log(err);
+				dispatch(authFail(err.response.data.error));
+			})		
+	}
+};
